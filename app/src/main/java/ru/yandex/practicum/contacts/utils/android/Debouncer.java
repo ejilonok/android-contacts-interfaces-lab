782b42ecd372372d7cf88f4bd01470f71a6264bd
ejilonok <awesome.ejilonok@yandex.ru>
2024-05-27 11:27:50 +0300

    Выполнена задача изменения класса Debouncer и проекта в общем в соответствии с заданием. Код вычищен для устранения warning'ов.

--- app/src/main/java/ru/yandex/practicum/contacts/utils/android/Debouncer.java
+++ app/src/main/java/ru/yandex/practicum/contacts/utils/android/Debouncer.java
@@ -4,8 +4,6 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 
-import ru.yandex.practicum.contacts.presentation.main.MainViewModel;
-
 import androidx.annotation.NonNull;
 
 public class Debouncer {
@@ -13,10 +11,10 @@ public class Debouncer {
     private static final int MESSAGE_ID = 1;
     private static final int DELAY = 500;
 
-    private final MainViewModel viewModel;
+    private final OnDebounceListener listener;
 
-    public Debouncer(MainViewModel viewModel) {
-        this.viewModel = viewModel;
+    public Debouncer(OnDebounceListener listener) {
+        this.listener = listener;
     }
 
     private final Handler handler = new Handler(Looper.getMainLooper()) {
@@ -37,8 +35,8 @@ public class Debouncer {
         handler.sendMessageDelayed(message, DELAY);
     }
 
-    // выполнить действие по прошествии 500мс, если нового события в течении 500мс не было отправлено
+    // выполнить действие по прошествии 500мс, если нового события в течении 500мс не было отправлено@Override
     private void doOnDebounce() {
-        viewModel.search();
+        listener.doOnDebounce();
     }
 }
